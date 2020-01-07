7aa97226fc5b97a246789a87eead7ce6c636bbcd
jsheradin <jsheradin@gmail.com>
2020-01-06 19:14:07 -0500

    Measure reference terrain deviation

--- lidar.py
+++ lidar.py
@@ -7,38 +7,63 @@ import logging
 import subprocess
 import threading
 import collections
-import time
-
-# Process LIDAR data and send to buffer
-def read_output(process, append, fov):
-    try:
-        for line in iter(process.stdout.readline, ""):
-            try:
-                meas = line.decode('utf-8').split(' ')
-                angle = float(meas[4])
-                dist = float(meas[6])
-                quality = float(meas[8])
-
-                if angle >= fov[0] and angle <= fov[1]:  # Within FOV
-                    append([angle, dist, quality])
-
-            except:
-                logging.debug("Bad lidar data")
-    except:
-        logging.error("No lidar data")
-        append([0, 0, 0]) # Dummy data
+from bisect import bisect_left
 
 # Create object to connect to LIDAR
 class lidar:
-    def __init__(self, buffer_size = 1000, field_of_view = [0, 360]):
-        logging.info("Starting lidar")
+    # Get distance from closest reference point
+    def get_deviation(self, measurement):
+        pos = bisect_left(self.ref_angles, measurement[0]) # Get position of closest angle, O(log n) sort time
+        return self.reference[pos][1] - measurement[1]
 
+    # Process LIDAR data and send to buffer
+    def read_output(self, process, append, fov):
+        try:
+            for line in iter(process.stdout.readline, ""):
+                try:
+                    meas = line.decode('utf-8').split(' ')
+                    angle = float(meas[4])
+                    dist = float(meas[6])
+                    quality = float(meas[8])
+                    dev = self.get_deviation([angle, dist])
+
+                    if fov[0] <= angle <= fov[1] and dist > 0:  # Within FOV and real reading
+                        append([angle, dist, dev, quality])
+
+                except:
+                    logging.debug("Bad lidar data")
+        except:
+            logging.error("No lidar data")
+            append([0, 0, 0])  # Dummy data
+
+    def load_ref(self):
+        # Load reference terrain (flat ground)
+        ref_file = open("lidar_ref.csv", "r+")  # Reference terrain file
+        self.reference = []
+        for line in ref_file.readlines():
+            line = line.split(',')
+            angle = float(line[0])
+            line = line[1].split('\n')
+            dist = float(line[0])
+            self.reference.append([angle, dist])
+        ref_file.close()
+        self.ref_angles = [item[0] for item in self.reference]
+
+    def __init__(self, buffer_size = 1200, field_of_view = [90, 270], testing=False):
+        logging.info("Starting lidar")
         self.info = "Starting"
 
         try:
             # This all obeys GIL so there *shouldn't* be race conditions
             self.buffer = collections.deque(maxlen=buffer_size) # Buffer to hold most recent values
-            self.proc = subprocess.Popen("/home/cam/CAM/lidar.o", stdout=subprocess.PIPE, stderr=subprocess.PIPE) # Iterator thread to keep buffer updated
+
+            if testing: # If running in IDE
+                path = "./lidar.o" # Pycharm runs in the project folder
+            else:
+                path = "/home/cam/CAM/lidar.o" # System service requires absolute path or path from CAM home folder
+
+            self.proc = subprocess.Popen(path, stdout=subprocess.PIPE,
+                                         stderr=subprocess.PIPE)  # Start lidar.o with piped output
 
             self.info = [] # Diagnostic info
             for i in range(6): # First 6 lines contain diagnostics
@@ -48,10 +73,17 @@ class lidar:
             for i in range(1000):
                 self.proc.stdout.readline()
 
+            self.load_ref()
+
             # Start iterator
-            self.t = threading.Thread(target=read_output, args=(self.proc, self.buffer.append, field_of_view))
+            self.t = threading.Thread(target=self.read_output, args=(self.proc, self.buffer.append, field_of_view))
             self.t.daemon = True
             self.t.start()
+
+            while len(self.buffer) < buffer_size: # Block until buffer is populated
+                pass
+
+            self.info = "READY"
         except:
             logging.error("LIDAR could not be started")
 
@@ -59,15 +91,52 @@ class lidar:
             self.info = "FAIL"
             self.buffer = [[0, 0, 0]]
 
+    # Read data for reference terrain and save
+    def calibrate(self):
+        if self.info != "READY": # Only calibrate if LIDAR is working
+            return "NO"
+
+        ref_file = open("lidar_ref.csv", "w+") # Empties file and opens for write
+        data = list(self.buffer)
+        data.sort() # Reference list must be sorted for use of bisect_left for search
+
+        for point in data:
+            ref_file.write("{},{}\n".format(point[0], point[1])) # CSV format
+        ref_file.close()
+
+        self.load_ref()
+
 
 # Testing
+def live_graph(prov_lid): # Live graph of LIDAR data
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as anim
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 2, 1, projection='polar')
+    ax2 = fig.add_subplot(1, 2, 2)
+
+    def animate(i):
+        data = prov_lid.buffer
+        angles = []
+        dists = []
+        devs = []
+        quals = []
+        for point in data:
+            angles.append((360 - point[0]) / 180 * 3.1415)
+            dists.append(point[1])
+            devs.append(point[2])
+            quals.append(point[3])
+        ax.clear()
+        ax2.clear()
+        ax.scatter(angles, dists, s=10, c=quals)
+        ax2.scatter(angles, devs, s=10)
+
+    ani = anim.FuncAnimation(fig, animate, interval=100)
+    plt.show()
+
+
 if __name__ == "__main__":
-    test = lidar(buffer_size=2000, field_of_view=[0, 180])
-    print("Initialized")
-    time.sleep(1)
-    print("Printing buffer")
-    print(test.info)
-    time.sleep(2)
-    print(test.buffer[0])
-    time.sleep(2)
-    print(test.buffer[0])
+    test = lidar(buffer_size=600, field_of_view=[90, 270], testing=True)
+    test.calibrate()
+    live_graph(test)
